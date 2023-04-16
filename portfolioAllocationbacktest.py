6d9d85f67145b0c365f282da4a2761c6bb79d4e0
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-16 20:34:55 +0200

    fixed a bug

--- portfolioAllocationbacktest.py
+++ portfolioAllocationbacktest.py
@@ -11,7 +11,7 @@ basedata = basedata.rename(columns= {"Open": members[0]})
 if (len(members) > 1):
     for i in range(1,len(members)):
         newdata = yf.Ticker(members[i]).history(period="max").reset_index()[["Date","Open"]]
-        newdata['Date']=pd.to_datetime(basedata['Date'])
+        newdata['Date']=pd.to_datetime(newdata['Date'])
         newdata = newdata.rename(columns= {"Open": members[i]})
         basedata = pd.merge(basedata, newdata, on="Date")
 
