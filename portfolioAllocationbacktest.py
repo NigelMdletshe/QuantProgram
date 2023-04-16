b87f045c7b20be4177ccbf3732f52bbafcc4e3fb
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-16 20:31:52 +0200

    added logic to extract more data

--- portfolioAllocationbacktest.py
+++ portfolioAllocationbacktest.py
@@ -8,4 +8,11 @@ basedata =yf.Ticker(members[0]).history(period="max").reset_index()[["Date","Ope
 basedata['Date']=pd.to_datetime(basedata['Date'])
 basedata = basedata.rename(columns= {"Open": members[0]})
 
+if (len(members) > 1):
+    for i in range(1,len(members)):
+        newdata = yf.Ticker(members[i]).history(period="max").reset_index()[["Date","Open"]]
+        newdata['Date']=pd.to_datetime(basedata['Date'])
+        newdata = newdata.rename(columns= {"Open": members[i]})
+        basedata = pd.merge(basedata, newdata, on="Date")
+
 print(basedata)
