452b488c05a16a1ba2adfca6ad155380ef0bf563
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-24 23:23:01 +0200

    resampled the data

--- SeasonalityinBitcoin.py
+++ SeasonalityinBitcoin.py
@@ -4,4 +4,8 @@ import plotly.graph_objects as go
 df = pd.read_csv("BCHAIN-MKPRU.csv")[["Date","Value"]]
 df["Date"] = pd.to_datetime(df["Date"])
 df.sort_values(by="Date",inplace = True)
-print(df)
+
+df = df.resample("1M").last()
+
+df = df.pct_change()
+df["Value"]*=100
