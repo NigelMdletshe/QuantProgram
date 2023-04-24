ae5f5476e2d4deea50f300df7f5097fb2659c925
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-25 00:03:39 +0200

    added a Heatwave plot

--- SeasonalityinBitcoin.py
+++ SeasonalityinBitcoin.py
@@ -20,7 +20,16 @@ z_vals = [trp(x,12) for x in z_vals]
 
 
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Sep','Oct','Nov','Dec']
-years = [str(x) for x in range(2009,2021)]
+years = [str(x) for x in range(2009,2022)]
 
+fig = go.Figure(data=go.Heatmap(
+    z = z_vals,
+    x = months,
+    y= years,
+    zmax=100,
+    zmin=-20,
+    zsmooth="best"
+))
 
-
+fig.show()
+#heat wave graph not shown, there is an error, check adblock and firewall
