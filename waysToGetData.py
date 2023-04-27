1c90fb3f99e54bd546c350f7753b459a1b8a8794
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-27 23:58:13 +0200

    added a resample function for different time frames

--- waysToGetData.py
+++ waysToGetData.py
@@ -3,6 +3,20 @@
 #cryptodatadownload.com
 
 import pandas as pd
-df = pd.read_csv("BTCUSD.csv")
+df = pd.read_csv("BTCUSD.csv") # replace daily data with 1min data
 df["date"] = pd.to_datetime(df["unix"],unit="s")
 print(df)
+
+#download 1min data
+
+df.set_index("date",inplace=True)
+
+#resample to 2min data
+df2 = df.resample("2T").add({
+    "open":"first",
+    "high":"max",
+    "low":"min",
+    "close":"last"
+})
+
+print(df2)
