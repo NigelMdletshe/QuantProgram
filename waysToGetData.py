2f58d9f7985f6d4ab2e5a1cb0dc9cee4d6e88f8b
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-28 00:09:38 +0200

    function to download from yfinance and save to csv

--- waysToGetData.py
+++ waysToGetData.py
@@ -1,9 +1,27 @@
 #data.Nasdaaq.com
 #kaggle.com for long term 1min bars
 #cryptodatadownload.com
+#Yfinance
 
 import pandas as pd
+import yfinance as yf
+
+
+df = yf.Ticker("BTC-USD").history(period="1d", interval="1m")
+print(df)
+#resample to 2min data
+df2 = df.resample("2T").agg({
+    "Open":"first",
+    "High":"max",
+    "Low":"min",
+    "Close":"last"
+})
+print(df2)
+
+df2.to_csv("testdata.csv")
+""" 
 df = pd.read_csv("BTCUSD.csv") # replace daily data with 1min data
+df.sort_values(by="unix", inplace = True)
 df["date"] = pd.to_datetime(df["unix"],unit="s")
 print(df)
 
@@ -12,7 +30,7 @@ print(df)
 df.set_index("date",inplace=True)
 
 #resample to 2min data
-df2 = df.resample("2T").add({
+df2 = df.resample("2T").agg({
     "open":"first",
     "high":"max",
     "low":"min",
@@ -20,3 +38,4 @@ df2 = df.resample("2T").add({
 })
 
 print(df2)
+"""
