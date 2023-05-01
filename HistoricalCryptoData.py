ec8e5256940650639708887121cdd10e3a347273
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-01 23:26:45 +0200

    works fine

--- HistoricalCryptoData.py
+++ HistoricalCryptoData.py
@@ -5,7 +5,7 @@ import pandas as pd
 import datetime
 
 start = "2021-11-01"
-end = "2021-11-02"
+end = "2021-11-10"
 
 dates = pd.date_range(start, end, freq = "12H")
 dates =[int(x.value/(10**9)) for x in list(dates)]
@@ -16,7 +16,7 @@ data = []
 for first, last in zip (dates, dates[1:]):
 
     parameters = {
-        "step":60,
+        "step":60, # change timeframe here
         "limit":1000,
         "start": first,
         "end":last
@@ -30,5 +30,12 @@ for first, last in zip (dates, dates[1:]):
 
 
 df = pd.DataFrame(data)
+df = df.drop_duplicates()
+df["timestamp"] = df["timestamp"].astype(int)
+df = df[ df["timestamp"] >= dates[0]]
+df = df[ df["timestamp"] < dates[-1]]
+
+df["date"] = pd.to_datetime(df["timestamp"], unit = "s")
 print(df)
 print(len(df))
+df.to_csv("data.csv")
