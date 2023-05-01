d0977c2b214b9f1b2fcf2156bb21c2f29ae59855
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-01 22:56:56 +0200

    added a for loop to iterate through eacg data

--- HistoricalCryptoData.py
+++ HistoricalCryptoData.py
@@ -5,17 +5,28 @@ import pandas as pd
 import datetime
 
 start = "2021-11-01"
-end = "2021-11-01"
+end = "2021-11-30"
+
+dates = pd.date_range(start, end)
+dates =[int(x.value/(10**9)) for x in list(dates)]
+print(dates)
 
 pair ="btcusd"
-parameters = {
-    "step":60,
-    "limit":10,
-}
-req = requests.get(f"https://www.bitstamp.net/api/v2/ohlc/{pair}",
+
+for first, last in zip (dates, dates[1:]):
+
+    parameters = {
+        "step":60,
+        "limit":10,
+        "start": first,
+        "end":last
+    }
+    req = requests.get(f"https://www.bitstamp.net/api/v2/ohlc/{pair}",
                 params= parameters )
 
 
-req = req.json()["data"]["ohlc"]
-df = pd.DataFrame(req)
-print(df)
+    req = req.json()["data"]["ohlc"]
+
+
+    df = pd.DataFrame(req)
+    print(df)
