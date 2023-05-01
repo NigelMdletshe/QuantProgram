f8afce01f03e2594f8ec24ac8e8694d427bfed53
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-01 22:37:25 +0200

    added a bitstamp request.

--- HistoricalCryptoData.py
+++ HistoricalCryptoData.py
@@ -3,3 +3,12 @@ import json
 import requests
 import pandas as pd
 import datetime
+
+pair ="btcusd"
+parameters = {
+    "step":60,
+    "limit":10,
+}
+req = requests.get(f"https://www.bitstamp.net/api/v2/ohlc/{pair}",
+                params= parameters )
+print(req)
