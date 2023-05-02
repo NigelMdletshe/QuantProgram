a93e3241d588457725e3e24b90374948e641f51f
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-02 23:35:53 +0200

    added packages

--- algoTradingInPython.py
+++ algoTradingInPython.py
@@ -1,3 +1,13 @@
 #1 Data Gathering
 #2 Backtesting 
-#3 
+#3 Execution Code
+#4 Deploy bot to Cloud
+#5 Trade analysis/Wrapup
+#Get Data from Bistamp.
+
+import json
+import requests
+import pandas as pd
+import datetime
+
+
