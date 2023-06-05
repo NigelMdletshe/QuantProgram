c841b20f19fd302507c6d9aa8417367ec8d16c89
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-06-05 11:13:15 +0200

    added a moving average

--- plotCandleSticks.py
+++ plotCandleSticks.py
@@ -4,6 +4,8 @@ import mplfinance as mpf
 
 df = yf.Ticker("BTC-USD").history(period="max")
 print(df)
+df["50ma"] = df["Open"].rolling(windows=50).mean()/1.5
+apds = [ mpf.make_addplt(df["50ma"])]
 # something wrong with the environment
 df = df.loc["2018-01-01":]
-mpf.plot(df, type = "candle", volume = True)
+mpf.plot(df, type = "candle", volume = True,mav=(10,3,20),addplot = apds)
