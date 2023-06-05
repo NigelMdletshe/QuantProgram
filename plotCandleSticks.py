c9897104ac1eccb93cd83dd85b8dda7284edbfb8
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-06-05 10:57:54 +0200

    plotting candle sticks

--- plotCandleSticks.py
+++ plotCandleSticks.py
@@ -5,3 +5,5 @@ import mplfinance as mpf
 df = yf.Ticker("BTC-USD").history(period="max")
 print(df)
 # something wrong with the environment
+df = df.loc["2018-01-01":]
+mpf.plot(df, type = "candle", volume = True)
