38ae11b6ba512aea96d872442ff5d4fcf08e1f22
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-06-05 11:24:22 +0200

    deleted an env

--- plotCandleSticks.py
+++ plotCandleSticks.py
@@ -5,7 +5,9 @@ import mplfinance as mpf
 df = yf.Ticker("BTC-USD").history(period="max")
 print(df)
 df["50ma"] = df["Open"].rolling(windows=50).mean()/1.5
-apds = [ mpf.make_addplt(df["50ma"])]
+df["ma"] = df["Open"].rolling(windows=50).mean()*1.5
+apds = [ mpf.make_addplt(df["50ma","ma"])]
 # something wrong with the environment
 df = df.loc["2018-01-01":]
 mpf.plot(df, type = "candle", volume = True,mav=(10,3,20),addplot = apds)
+
